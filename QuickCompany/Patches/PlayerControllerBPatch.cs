741d750054dcbaed83c620b70660da1cf99f276b
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2024-09-27 03:35:28 +0300

    Add project files.

---
+++ QuickCompany/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using GameNetcodeStuff;
+
+namespace QuickCompany.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patchUpdate(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ QuickCompany/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using QuickCompany.Patches;
+using HarmonyLib;
+
+namespace QuickCompany
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class QuickCompanyBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Phantom.QuickCompany";
+        private const string modName = "QuickCompany";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static QuickCompanyBase Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Quick Company is working.");
+
+            harmony.PatchAll(typeof(QuickCompanyBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
